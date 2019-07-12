5fba82cfd7c708fa0e93455990b48cc7f904256e
Fagner Freitas <fagner@universal.local>
2019-07-12 18:02:04 -0300

    Autenticando as rotas

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { BrowserRouter,Switch, Router, Route,  } from "react-router-dom"
+import React,{Component} from 'react';
+import { BrowserRouter,Switch, Router, Route, Redirect } from "react-router-dom"
 
 import Home from './pages/Home';
 import Login from './pages/Login';
+
+const isAuth = ()=> false; 
+
+const PrivateRoute = ({component: Component, ...rest}) =>(
+    <Route
+    {...rest}
+    render={ props =>
+
+        isAuth() ? ( <Component {...props} /> ) : (<Redirect to={{pathname: "/", state : {from : props.location }}} />
+        )
+    }
+    />
+);
+
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Home}  />
-            <Route  path="/login" component={Login}  />
+            <PrivateRoute  path="/login" component={Login}  />
         </Switch>
     </BrowserRouter>
 )
