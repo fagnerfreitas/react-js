56952b1bfd58ac7c639ae4724d4da57b4ea7dbb3
fagnerfreitas <fagner.hf@hotmail.com>
2019-07-16 23:49:07 -0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -41,7 +41,7 @@ class Home extends Component {
                 //  'Content-Type':'application/x-www-form-urlencoded',
             },
             
-              mode:'no-cors',
+              mode:'cors',
             // cache: 'default' ,
         })
             .then(res => res.json())
